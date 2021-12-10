7e324d883f5715f643f603c908df0d7f89e07055
NeriGB <neriguzman10@gmail.com>
2021-12-10 10:42:37 -0600

    prueba

---
+++ src/models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  nombre: String,
+  precio: String,
+  
+});
+
+module.exports = mongoose.model('product', userSchema);
